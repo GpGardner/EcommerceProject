b91eb33a39bf8af2be21854fca9282a465d7ae97
George Gardner <gpgardner@yahoo.com>
2020-02-21 17:52:52 -0500

    Initial commit

---
+++ src/main/java/com/tts/EcommerceProject/controller/CartController.java
@@ -0,0 +1,17 @@
+package com.tts.EcommerceProject.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.ElementCollection;
+
+public class CartController {
+	
+//	@ElementCollection
+//	private void cart (Product product, int quantity) {
+//		Map <Product, Integer> cart = new HashMap<>();
+//		
+//		
+//	}
+//		
+	}
