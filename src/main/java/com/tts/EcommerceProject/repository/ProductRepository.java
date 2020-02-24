fc8b6d2d845cd1185c84104b3ddd5cc550e4a69c
George Gardner <gpgardner@yahoo.com>
2020-02-24 10:17:55 -0500

    Reorganized project to properly orient necessary documents

--- src/main/java/com/tts/EcommerceProject/repository/ProductRepository.java
+++ src/main/java/com/tts/EcommerceProject/repository/ProductRepository.java
@@ -9,18 +9,16 @@ import org.springframework.stereotype.Repository;
 import com.tts.EcommerceProject.model.Product;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long>{
-	
-	 List<Product> findAll();
-     Product findById(long id);
-     List<Product> findByBrand(String brand);
-     List<Product> findByCategory(String category);
-     List<Product> findByBrandAndCategory(String brand, String category);
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findAll();
+    Product findById(long id);
+    List<Product> findByBrand(String brand);
+    List<Product> findByCategory(String category);
+    List<Product> findByBrandAndCategory(String brand, String category);
 
-     @Query("SELECT DISTINCT p.brand FROM Product p")
-     List<String> findDistinctBrands();
+    @Query("SELECT DISTINCT p.brand FROM Product p")
+    List<String> findDistinctBrands();
 
-     @Query("SELECT DISTINCT p.category FROM Product p")
-     List<String> findDistinctCategories();
+    @Query("SELECT DISTINCT p.category FROM Product p")
+    List<String> findDistinctCategories();
 }
-
